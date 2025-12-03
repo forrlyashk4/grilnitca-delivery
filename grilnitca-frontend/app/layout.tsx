a83c4d596704d2eb16b8e819450540155fb546bc
forrlyashk4 <nikitarudometof@gmail.com>
2025-12-03 18:40:11 +0300

    init commit (v2 :))

---
+++ grilnitca-frontend/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Грильница | Главная",
+  description: "Доставка вкусной еды на дом",
+};
+
+const nunito = Nunito({
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <body className={nunito.className}>
+        <main className="min-h-screen">{children}</main>
+      </body>
+    </html>
+  );
+}
